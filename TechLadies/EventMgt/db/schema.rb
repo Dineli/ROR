cb87cb3d70bb89544c8d08f96b7ec2de1ee63b23
Dineli <dinelikarunathilaka@gmail.com>
2016-09-01 17:30:26 +0800

    modified

--- TechLadies/EventMgt/db/schema.rb
+++ TechLadies/EventMgt/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160827051326) do
+ActiveRecord::Schema.define(version: 20160901022158) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title",              limit: 255
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20160827051326) do
     t.string   "image_content_type", limit: 255
     t.integer  "image_file_size",    limit: 4
     t.datetime "image_updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",            limit: 255
+    t.string   "email",           limit: 255
+    t.string   "password_digest", limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
 end
