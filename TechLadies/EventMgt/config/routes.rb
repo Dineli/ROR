e078485b32e829d64ec78041aba58ced4897fb36
Dineli <dinelikarunathilaka@gmail.com>
2016-09-05 12:38:25 +0800

    removed commented code

--- TechLadies/EventMgt/config/routes.rb
+++ TechLadies/EventMgt/config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
   resources :event
-#  , :path =>"eVeNtZ"
   
   root 'event#home'
   
   get 'eventz' => "event#home", as: :home 
   get 'eventz/contact' => "event#contact", as: :contact 
   get 'eventz/list' => "event#index", as: :index
-#  get 'eventz/info' => "event#show", as: :show
 
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
